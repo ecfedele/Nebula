cf95f19ad5a974939ec3a7c12001cbfe45b38d66
Connor C <connorclarke@comcast.net>
2023-07-18 14:58:24 -0700

    Updating ALU_32 to do operations only when alu_in is high and when the clock edge is rising.

--- fpga/Alu_32.vhd
+++ fpga/Alu_32.vhd
@@ -23,7 +23,7 @@ use     IEEE.STD_LOGIC_1164.ALL;
 use     IEEE.NUMERIC_STD.ALL;
 
 ---------------------------------------------------------------------------------------------------
--- Module:      Alu_32                                                                              --
+-- Module:      Alu_32                                                                           --
 -- Description: Implements an ALU tailored to the RISC-V specification.                          --
 --                                                                                               --
 --                                                                                               --
@@ -31,6 +31,7 @@ use     IEEE.NUMERIC_STD.ALL;
 -- Inputs:      input_a, input_b   (REG_WIDTH) Inputs to perform operation on.		         --
 --              clk                (1)         Clock signal input                                --
 --              n_rst              (1)         Asynchronous reset signal (active LOW)            --
+--              din_regwr           (1)	       Register write signal				 --
 --              alu_in             (1)	       Indicates an integer arithmetic (ALU) instruction --    
 --              ALUOp              (7)         Opp code of the operation to be performed         --
 --                                                                                               --
@@ -40,11 +41,11 @@ use     IEEE.NUMERIC_STD.ALL;
 entity Alu_32 is 
     generic(REG_WIDTH : INTEGER := 32);
     port(
-        input_a, input_b            : in  STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
-        clk, n_rst, din_regwr	    : in  STD_LOGIC;
-        ALUOp			    : in  STD_LOGIC_VECTOR(6 downto 0);
-        dout_regwr		    : out STD_LOGIC;
-        dout_result            	    : out STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0)
+        input_a, input_b            	: in  STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
+        clk, n_rst, din_regwr, alu_in	: in  STD_LOGIC;
+        ALUOp			    	: in  STD_LOGIC_VECTOR(6 downto 0);
+        dout_regwr		    	: out STD_LOGIC;
+        dout_result            	    	: out STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0)
     );
 end entity Alu_32;
 
@@ -53,15 +54,17 @@ architecture ALU of ALU_32 is
 	signal ALU_TMP_Result : STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
 
 begin
-    process(input_a,input_b,ALUOp)
+    process(clk, alu_in)
 	begin
-		case(ALUOp) is
-			when "0000000" => --Addition
-     				ALU_TMP_RESULT(DATA_WIDTH-1 downto 0) <= signed(input_a) + signed(input_b);
-     			when "0100000" => -- Sub
-				ALU_TMP_RESULT <= signed(input_a) - signed(input_b);
-			when others => null;
-		end case;
+		if alu_in = "1" and rising_edge(clk) then 
+			case(ALUOp) is
+				when "0000000" => --Addition
+     					ALU_TMP_RESULT(DATA_WIDTH-1 downto 0) <= signed(input_a) + signed(input_b);
+     				when "0100000" => -- Sub
+					ALU_TMP_RESULT <= signed(input_a) - signed(input_b);
+				when others => null;
+			end case;
+		end if;
 	end process;
 	dout_result <= ALU_TMP_RESULT;
 	dout_regwr <= din_regwr;
