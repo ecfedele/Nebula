e112a681b1d2ee2fa16becb086c49fb6686c9887
ecfedele <118453623+ecfedele@users.noreply.github.com>
2023-07-19 00:59:00 -0400

    Update Alu_32.vhd
    Tidied up a bit; added explicit case back to STD_LOGIC_VECTOR

--- fpga/Alu_32.vhd
+++ fpga/Alu_32.vhd
@@ -1,7 +1,7 @@
 ---------------------------------------------------------------------------------------------------
 -- Title          : Arithmetic Logic unit, 32-bit Integer for RV32G                              --
 -- Project        : Nebula RV32 Core                                                             --
--- Filename       : Alu_32.vhd                                                                      --
+-- Filename       : Alu_32.vhd                                                                   --
 -- Description    : Implements the main ALU.                                                     --
 --                                                                                               --
 -- Main Author    : Connor Clarke                                                                --
@@ -28,44 +28,44 @@ use     IEEE.NUMERIC_STD.ALL;
 --                                                                                               --
 --                                                                                               --
 -- Parameters:  REG_WIDTH          (INTEGER)   Configures the register's data width              --
--- Inputs:      input_a, input_b   (REG_WIDTH) Inputs to perform operation on.		         --
+-- Inputs:      input_a, input_b   (REG_WIDTH) Inputs to perform operation on.                   --
 --              clk                (1)         Clock signal input                                --
 --              n_rst              (1)         Asynchronous reset signal (active LOW)            --
---              din_regwr           (1)	       Register write signal				 --
---              alu_in             (1)	       Indicates an integer arithmetic (ALU) instruction --    
+--              din_regwr          (1)         Register write signal                             --
+--              alu_in             (1)         Indicates an integer arithmetic (ALU) instruction --    
 --              ALUOp              (7)         Opp code of the operation to be performed         --
 --                                                                                               --
--- Outputs:     dout_regwr	   (1)	       Write results to register (active LOW) 		 --							 --
--- 		dout_result        (REG_WIDTH) Result of Arithmetic instruction              	 --
+-- Outputs:     dout_regwr         (1)         Write results to register (active LOW)            --                             --
+--              dout_result        (REG_WIDTH) Result of Arithmetic instruction                  --
 ---------------------------------------------------------------------------------------------------
 entity Alu_32 is 
     generic(REG_WIDTH : INTEGER := 32);
     port(
-        input_a, input_b            	: in  STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
-        clk, n_rst, din_regwr, alu_in	: in  STD_LOGIC;
-        ALUOp			    	: in  STD_LOGIC_VECTOR(6 downto 0);
-        dout_regwr		    	: out STD_LOGIC;
-        dout_result            	    	: out STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0)
+        input_a, input_b              : in  STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
+        clk, n_rst, din_regwr, alu_in : in  STD_LOGIC;
+        ALUOp                         : in  STD_LOGIC_VECTOR(6 downto 0);
+        dout_regwr                    : out STD_LOGIC;
+        dout_result                   : out STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0)
     );
 end entity Alu_32;
 
 architecture ALU of ALU_32 is
 
-	signal ALU_TMP_Result : STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
+    signal ALU_TMP_Result : STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
 
 begin
     process(clk, alu_in)
-	begin
-		if alu_in = "1" and rising_edge(clk) then 
-			case(ALUOp) is
-				when "0000000" => --Addition
-     					ALU_TMP_RESULT(DATA_WIDTH-1 downto 0) <= signed(input_a) + signed(input_b);
-     				when "0100000" => -- Sub
-					ALU_TMP_RESULT <= signed(input_a) - signed(input_b);
-				when others => null;
-			end case;
-		end if;
-	end process;
-	dout_result <= ALU_TMP_RESULT;
-	dout_regwr <= din_regwr;
+    begin
+        if alu_in = "1" and rising_edge(clk) then 
+            case(ALUOp) is
+                when "0000000" => --Addition
+                    ALU_TMP_Result <= STD_LOGIC_VECTOR(SIGNED(input_a) + SIGNED(input_b));
+                when "0100000" => -- Sub
+                    ALU_TMP_RESULT <= STD_LOGIC_VECTOR(SIGNED(input_a) - SIGNED(input_b));
+                when others => null;
+            end case;
+        end if;
+    end process;
+    dout_result <= ALU_TMP_RESULT;
+    dout_regwr  <= din_regwr;
 end architecture ALU;
